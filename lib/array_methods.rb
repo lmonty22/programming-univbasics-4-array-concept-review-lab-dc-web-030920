9be098b4371d24891f2710097d54c9e37a4ed570
Lindsay Montgomery <lindsay.montgomery@olympian.org>
2020-02-24 20:35:37 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -22,4 +22,17 @@ def find_max_value(array)
   max_value
 end 
 
+def find_min_value (array)
+  index = 0 
+  min_value = array[0]
+  while index < array.length do 
+    if array[index] < min_value 
+      min_value = array[index]
+    end 
+    index += 1 
+  end 
+  min_value
+end 
+
+
   
