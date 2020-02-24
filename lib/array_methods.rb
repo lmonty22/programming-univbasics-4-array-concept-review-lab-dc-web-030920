99b2fdf6fd302e68dcc2e082d19b4c54666b7549
Lindsay Montgomery <lindsay.montgomery@olympian.org>
2020-02-24 20:33:04 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,1 +1,11 @@
 
+def find_element_index (array, value_to_find)
+  index = 0 
+  while index < array.length do 
+    if array[index] == value_to_find
+      return index 
+    else 
+      index += 1 
+    end 
+  end 
+end 
